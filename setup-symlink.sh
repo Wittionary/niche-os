badd486f9fec7934c578ec873170f66700411457
Witt Allen <wittionary@users.noreply.github.com>
2024-04-15 14:14:37 -0500

    bootstrap setup on new nix OS install?

---
+++ setup-symlink.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Points the system directory over to the git-managed configurations
+
+NIXOS_DIR="/etc/nixos/"
+GIT_DIR="~/git/niche-os/nixos/"
+CONFIGS="configuration.nix hardware-configuration.nix"
+
+for config in $CONFIGS; do
+	FILEPATH="$NIXOS_DIR$config"
+	
+	# is present but not as a symlink
+    if [[ -f "$FILEPATH" && ! ( -L "$FILEPATH" ) ]]; then
+        rm -v "$FILEPATH"
+
+	# is a symlink
+    elif [[ -L "$FILEPATH" ]]; then
+        echo "$FILEPATH is already symlinked"
+    else
+        ln --symbolic --verbose "$GIT_DIR$config" "$FILEPATH" && echo "$FILEPATH is now a symlink"
+    fi
+done
