24aeed5002570ba1f83ae17f276b03e092132ca5
Witt Allen <wittionary@users.noreply.github.com>
2024-04-15 13:19:16 -0500

    Added script so I don't have to touch client machine

---
+++ continuous-download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the path to the Git repository
+repo_url="https://github.com/Wittionary/niche-os"
+
+while true; do
+    # Get the current time in HH:MM format
+    current_time=$(date +"%H:%M")
+
+    # Pull the Git repository
+    git pull -C "$repo_url" 
+
+    # Check if the repository has changed
+    if [[ $(git pull "$repo_url") == "Already up to date." ]]; then
+        echo "$current_time - repo hasn't changed"
+    else
+        echo "$current_time - changes found. Running nixos-rebuild switch"
+        sudo nixos-rebuild switch
+    fi
+
+    # Wait for 1 minute before checking again
+    sleep 1m
+done
+
