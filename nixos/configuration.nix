60b584ef8120e7bf7d970c105093727d9197bfc9
Witt Allen <wittionary@users.noreply.github.com>
2024-04-17 20:26:59 -0500

    Enable ssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -161,7 +161,7 @@ programs.git = {
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
