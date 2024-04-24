107ec20ad87ec3c9bb0f7867c0689c6f9f662f87
Witt Allen <Wittionary@users.noreply.github.com>
2024-04-24 16:06:15 -0500

    Attempting WSL config

--- nixos/wsl.nix
+++ nixos/wsl.nix
@@ -14,14 +14,27 @@
   ];
 
   wsl.enable = true;
-  wsl.defaultUser = "nixos";
+  wsl.defaultUser = "wittnix";
 
+  environment.systemPackages = with pkgs; [
+    #_1password
+    curl
+    file
+    git-credential-manager
+    home-manager
+    jq
 
+    nmap
+    podman
+    podman-compose
+    vim 
+    wget
+  ];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  # system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
