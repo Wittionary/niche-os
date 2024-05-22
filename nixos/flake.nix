2a5dfc1fc1f179449872054963c36c6ef608e95a
Witt Allen <wittionary@users.noreply.github.com>
2024-05-22 13:10:15 -0500

    Added flake.nix

---
+++ nixos/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-unstable branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.snowmachine = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
