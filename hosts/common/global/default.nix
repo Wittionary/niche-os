1a3e58457e79e0b582e864bc55488f342bc0cce8
Witt Allen <Wittionary@users.noreply.github.com>
2026-01-22 18:07:03 -0600

    Adds Mullvad VPN, i2p, Tor browser, and SimpleX chat; code formatted (#22)

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -193,6 +193,12 @@
     nmap
     openssl
 
+    # privacy / anonymity-based
+    i2p # https://geti2p.net/en/about/intro
+    mullvad # CLI tool for the VPN client
+    simplex-chat-desktop
+    tor-browser
+
     # system
 
     # terminal
