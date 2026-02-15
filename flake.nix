e48ece8d97863076e34dc12c01175363cd661c2b
Witt Allen <Wittionary@users.noreply.github.com>
2026-02-14 22:18:19 -0600

    Feature/update flake lock (#30)
    * Commented this out because it wouldn't build; upstream issue with StevenBlack `hosts`
    
    * Updated flake lock
    
    * Resolving flake lock update build errors
    
    * Changed DNSSEC

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
-  description = "Your new nix config";
+  description = "Wittionary's nixOS configuration";
 
-  inputs = { # "inputs" defines all the dependencies of this flake
+  inputs = {
+    # "inputs" defines all the dependencies of this flake
     # NixOS official package source
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
+
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -21,86 +22,89 @@
     };
 
     # hosts file provider - https://github.com/StevenBlack/hosts#nix-flake
-    hosts.url = "github:StevenBlack/hosts/master";
+    # stevenblack-hosts.url = "github:StevenBlack/hosts/master";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    hosts,
-    nixos-hardware,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-  in {
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # Yoga laptop - mainly for nixOS development
-      snowmachine = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [
-          ./hosts/snowmachine
-          hosts.nixosModule
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      # stevenblack-hosts,
+      nixos-hardware,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+    in
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # Yoga laptop - mainly for nixOS development
+        snowmachine = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [
+            ./hosts/snowmachine
+            # stevenblack-hosts.nixosModule
+          ];
+        };
+
+        # PC desktop
+        starmachine = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/starmachine
+            # stevenblack-hosts.nixosModule
+            # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+            nixos-hardware.nixosModules.common-cpu-amd
+            nixos-hardware.nixosModules.common-gpu-nvidia-nonprime # the real graphics card
+            nixos-hardware.nixosModules.common-pc
+            # nixos-hardware.nixosModules.common-pc-ssd # not sure if needed
+            # nixos-hardware.nixosModules.common-hidpi # not sure if needed
+          ];
+        };
+
+        # WSL terminals
+        stormtrooper = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [ ./hosts/stormtrooper ];
+        };
       };
 
-	    # PC desktop
-      starmachine = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [ 
-          ./hosts/starmachine
-          hosts.nixosModule
-          # add your model from this list: https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-gpu-nvidia-nonprime # the real graphics card
-          nixos-hardware.nixosModules.common-pc
-          # nixos-hardware.nixosModules.common-pc-ssd # not sure if needed
-          # nixos-hardware.nixosModules.common-hidpi # not sure if needed
-        ];
-      };
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        # Yoga laptop - mainly for nixOS development
+        "witt@snowmachine" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home/global
+            ./home/snowmachine.nix
+          ];
+        };
 
-      # WSL terminals
-      stormtrooper = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [ ./hosts/stormtrooper ];
+        # PC Desktop
+        "witt@starmachine" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pk>
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home/global
+            ./home/starmachine.nix
+          ];
+        };
+
+        # WSL terminals
+        "witt@stormtrooper" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs outputs; };
+          modules = [
+            ./home/global
+            ./home/stormtrooper.nix
+          ];
+        };
       };
     };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # Yoga laptop - mainly for nixOS development
-      "witt@snowmachine" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/snowmachine.nix
-        ];
-      };
-
-      # PC Desktop
-      "witt@starmachine" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pk>
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/starmachine.nix
-        ];
-      };
-
-      # WSL terminals
-      "witt@stormtrooper" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/stormtrooper.nix
-        ];
-      };
-    };
-  };
 }
