610605ccd62be521e4e84295b71f25faa5314ce6
Witt <wittionary@users.noreply.github.com>
2026-09-05 10:41:06 -0500

    fixed flake.nix rebase for the last time(?)

--- flake.nix
+++ flake.nix
@@ -84,37 +84,25 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # Yoga laptop - mainly for nixOS development
-      "witt@snowmachine" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/snowmachine.nix
-        ];
-      };
+        # Yoga laptop - mainly for nixOS development
+        "witt@snowmachine" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = {inherit inputs outputs;};
+          modules = [
+            ./home/global
+            ./home/snowmachine.nix
+          ];
+        };
 
-      # hacktop
-      "witt@hacktop" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' insta>
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/hacktop.nix
-        ];
-      };
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # Yoga laptop - mainly for nixOS development
-      "witt@snowmachine" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          ./home/global
-          ./home/snowmachine.nix
-        ];
-      };
+        # hacktop
+        "witt@hacktop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' insta>
+          extraSpecialArgs = {inherit inputs outputs;};
+          modules = [
+            ./home/global
+            ./home/hacktop.nix
+          ];
+        };
 
         # PC Desktop
         "witt@starmachine" = home-manager.lib.homeManagerConfiguration {
@@ -136,6 +124,5 @@
           ];
         };
       };
-    };
   };
 }
