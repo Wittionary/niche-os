2183f9cad1e468fddaced26769dd402d141997ad
Witt <wittionary@users.noreply.github.com>
2026-09-04 21:15:40 -0500

    updated config stateVersion

--- hosts/hacktop/default.nix
+++ hosts/hacktop/default.nix
@@ -49,5 +49,5 @@
   # security exceptions -------------
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "24.11";
+  system.stateVersion = "26.05";
 }
--- hosts/hacktop/hardware-configuration.nix
+++ hosts/hacktop/hardware-configuration.nix
@@ -10,25 +10,23 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ebe1378c-1470-41b3-b0c6-38a5938a7f6a";
+    { device = "/dev/mapper/luks-f5c99c28-4e8f-4507-ae3b-9ed27ab6ccde";
       fsType = "ext4";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/23f1411b-445d-4a05-933f-101ca821ae01"; }
-    ];
+  boot.initrd.luks.devices."luks-f5c99c28-4e8f-4507-ae3b-9ed27ab6ccde".device = "/dev/disk/by-uuid/f5c99c28-4e8f-4507-ae3b-9ed27ab6ccde";
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/F9A1-0BA7";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
